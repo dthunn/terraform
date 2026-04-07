fbdc22edea1af5a3c60614899eaacb8834674a08
Dylan Thunn <49996239+dthunn@users.noreply.github.com>
2026-04-07 13:07:06 -0600

    basics

--- first_ec2.tf
+++ first_ec2.tf
@@ -7,7 +7,7 @@ provider "aws" {
   secret_key = var.AWS_SECRET_KEY
 }    
 
-resource "aws_instance" "myec2" {
+resource "aws_instance" "ec2_test" {
     ami = "ami-00c39f71452c08778"
     instance_type = "t2.micro"
 }
