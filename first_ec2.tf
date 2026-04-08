1839976cd4546517d8ee3b67d16f3df0c4bc31b0
Dylan Thunn <49996239+dthunn@users.noreply.github.com>
2026-04-08 12:25:27 -0600

    basics

--- first_ec2.tf
+++ first_ec2.tf
@@ -1,13 +1,5 @@
-variable "AWS_ACCESS_KEY" {}
-variable "AWS_SECRET_KEY" {}     
 
-provider "aws" {                                                                                                                                                        
-  region     = "us-east-1"                                       
-  access_key = var.AWS_ACCESS_KEY
-  secret_key = var.AWS_SECRET_KEY
-}    
-
-resource "aws_instance" "ec2_test" {
-    ami = "ami-00c39f71452c08778"
-    instance_type = "t2.micro"
+resource "aws_instance" "myec2" {
+    ami = "ami-043ab4148b7bb33e9"
+    instance_type = "t3.micro"
 }
